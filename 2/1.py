b3efca8b58bf01c328a4a347349b9f86b4dfa88f
Izzy Spering <43628079+IzzyBoop@users.noreply.github.com>
2021-12-03 22:27:18 -0500

    World 2-1

---
+++ 2/1.py
@@ -0,0 +1,22 @@
+#im doing my best, okay?
+
+horizontalPos = 0
+depth = 0
+
+fileObj = open('2/input.txt', "r")
+listyBoi = fileObj.read().splitlines()
+fileObj.close()
+
+for f in listyBoi:
+    split = f.split()
+    direction = split[0]
+    amount = int(split[1])
+
+    if direction == "forward":
+        horizontalPos += amount
+    elif direction == "down":
+        depth += amount
+    elif direction == "up":
+        depth -= amount
+
+print(horizontalPos*depth)
